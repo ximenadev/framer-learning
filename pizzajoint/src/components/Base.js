ab048aa708ee8a9c0da3698d1699e4263ccb091a
Ximena Pérez <hola@ximena.dev>
2021-11-02 16:16:27 -0600

    lessons 1 to 6 done

---
+++ pizzajoint/src/components/Base.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const Base = ({ addBase, pizza }) => {
+  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+
+  return (
+    <motion.div className="base container"
+      initial={{ x: '100vw'}}
+      animate={{ x: 0}}
+      transition={{ type: 'spring', stiffness: 50, delay: 0.5 }}
+    >
+
+      <h3>Step 1: Choose Your Base</h3>
+      <ul>
+        {bases.map(base => {
+          let spanClass = pizza.base === base ? 'active' : '';
+          return (
+            <motion.li key={base} onClick={() => addBase(base)}
+              whileHover={{ scale: 1.3, color:'#F8E112', originX: 0 }}
+              transition={{ type:'spring', stiffness: 300 }}
+            >
+              <span className={spanClass}>{ base }</span>
+            </motion.li>
+          )
+        })}
+      </ul>
+
+      {pizza.base && (
+        <motion.div className="next"
+          initial={{ x: '-100vw' }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 40 }}
+        >
+          <Link to="/toppings">
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: "0px 0px 8px rgb(255,255,255)",
+                boxShadow: "0px 0px 8px rgb(255,255,255)",
+              }}>
+                Next
+            </motion.button>
+          </Link>
+        </motion.div>
+      )}
+
+    </motion.div>
+  )
+}
+
+export default Base;
---
+++ pizzajoint/src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const Home = () => {
+  return (
+    <motion.div className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 1 }}
+    >
+      <motion.h2>
+        Welcome to Pizza Joint
+      </motion.h2>
+      <Link to="/base">
+        <motion.button
+          whileHover={{ 
+            scale: 1.1, 
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}
+        >
+          Create Your Pizza
+        </motion.button>
+      </Link>
+    </motion.div>
+  )
+}
+
+export default Home;
