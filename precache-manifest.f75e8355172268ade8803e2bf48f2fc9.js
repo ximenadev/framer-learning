e27a0aa28849cb11f423507ccd93b2cb5d99c330
Ximena Pérez <hola@ximena.dev>
2021-11-16 22:04:11 -0600

    Updates

---
+++ precache-manifest.f75e8355172268ade8803e2bf48f2fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8459c488b789d22dff47591aee4bf17",
+    "url": "/framer-learning/index.html"
+  },
+  {
+    "revision": "60296ee9e4d46896581a",
+    "url": "/framer-learning/static/css/main.0b63eb0a.chunk.css"
+  },
+  {
+    "revision": "3b6d3d654ce7ef1e4e9c",
+    "url": "/framer-learning/static/js/2.3c5c3889.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-learning/static/js/2.3c5c3889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60296ee9e4d46896581a",
+    "url": "/framer-learning/static/js/main.8c7e8f57.chunk.js"
+  },
+  {
+    "revision": "b14ee2f6c8f157538e58",
+    "url": "/framer-learning/static/js/runtime-main.6b973f4b.js"
+  }
+]);
